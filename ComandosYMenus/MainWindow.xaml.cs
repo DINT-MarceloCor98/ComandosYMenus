6b83fb3732873c8f8893fa2adc16f32c67d5d589
MarceloCor98 <ma.cordero@iesdoctorbalmis.com>
2019-12-04 08:49:23 +0100

    Agregar archivos de proyecto.

---
+++ ComandosYMenus/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ComandosYMenus
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> lista;
+        string itemSeleccionado;
+        public MainWindow()
+        {
+            
+            lista = new ObservableCollection<string>();
+            InitializeComponent();
+            Hora.Text = DateTime.Now.ToLongTimeString();
+            itemSeleccionado = "";
+            listaElementos.DataContext = lista;
+            VaciarButton.Command = MisComandos.Clear;
+            PegarButton.Command = ApplicationCommands.Paste;
+            CopiarButton.Command = ApplicationCommands.Copy;
+            crearTemporizador();
+        }
+
+        public void crearTemporizador()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Hora.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            lista.Add("Item añadido a las " + DateTime.Now.ToLongTimeString());            
+        }
+
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = lista.Count < 10;
+        }
+
+        private void Clear_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            lista.Clear();
+        }
+
+        private void Clear_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (lista.Count > 0)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+
+        private void Salir_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Salir_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Copy_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            itemSeleccionado = listaElementos.SelectedItem.ToString();            
+        }
+
+        private void Copy_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = listaElementos.SelectedItem != null;
+        }
+
+        private void Paste_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            lista.Add(itemSeleccionado);
+            itemSeleccionado = "";
+        }
+
+        private void Paste_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            
+            e.CanExecute = itemSeleccionado != "";
+        }
+
+
+    }
+}
---
+++ ComandosYMenus/MisComandos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ComandosYMenus
+{
+    public static class MisComandos
+    {
+        public static readonly RoutedUICommand Clear = new RoutedUICommand
+            (
+                "Clear", "Clear", typeof(MisComandos), new InputGestureCollection()
+                {
+                    new KeyGesture(Key.R,ModifierKeys.Control)
+                }
+            );
+
+        public static readonly RoutedUICommand Salir = new RoutedUICommand
+            (
+                "Salir", "Salir", typeof(MisComandos), new InputGestureCollection()
+                {
+                    new KeyGesture(Key.S,ModifierKeys.Control)
+                }
+            );
+    }
+}
+
